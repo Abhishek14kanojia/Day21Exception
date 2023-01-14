6ed2cb6f9de6bca19c81a50fec357fa0a371a4a9
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2023-01-15 01:03:37 +0530

    UC3

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -5,7 +5,7 @@ public class MoodAnalyzer {
 	private String message;
 
 
-	public MoodAnalyzer(String message) {
+	public MoodAnalyzer(String message) throws MoodAnalyzerException{
 		this.message = message;
 	}
 	public  MoodAnalyzer() {
@@ -19,7 +19,7 @@ public class MoodAnalyzer {
 		this.message = message;
 	}
 	
-	 public String analyseMood() {
+	 public String analyseMood() throws  MoodAnalyzerException{
 	        try {
 	            if (message.toLowerCase().contains("sad")) {
 	                return "Sad";
@@ -29,8 +29,7 @@ public class MoodAnalyzer {
 	                return "Any";
 	            }
 	        } catch (NullPointerException e) {
-	            return "Invalid Mood";
+	        	throw new  MoodAnalyzerException ("Exception found");
 	        }
 	 }
-
 }
---
+++ MoodAnalyzerException.java
@@ -0,0 +1,8 @@
+package com.day21Exception;
+
+public class MoodAnalyzerException extends Exception {
+	public   MoodAnalyzerException(String message) {
+		super(message);
+	}
+	
+}
